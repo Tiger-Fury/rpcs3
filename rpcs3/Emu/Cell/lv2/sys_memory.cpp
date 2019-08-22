a683484949dccd2386145e160e8a0723a35a95e0
RipleyTom <RipleyTom@users.noreply.github.com>
2019-08-23 01:21:25 +0200

    sys_memory_allocate gives allocated address

--- rpcs3/Emu/Cell/lv2/sys_memory.cpp
+++ rpcs3/Emu/Cell/lv2/sys_memory.cpp
@@ -23,8 +23,6 @@ error_code sys_memory_allocate(u32 size, u64 flags, vm::ptr<u32> alloc_addr)
 {
 	vm::temporary_unlock();
 
-	sys_memory.warning("sys_memory_allocate(size=0x%x, flags=0x%llx, alloc_addr=*0x%x)", size, flags, alloc_addr);
-
 	if (!size)
 	{
 		return {CELL_EALIGN, size};
@@ -64,6 +62,7 @@ error_code sys_memory_allocate(u32 size, u64 flags, vm::ptr<u32> alloc_addr)
 			if (alloc_addr)
 			{
 				*alloc_addr = addr;
+				sys_memory.warning("sys_memory_allocate(size=0x%x, flags=0x%llx, alloc_addr=*0x%x, *alloc_addr=0x%x)", size, flags, alloc_addr, *alloc_addr);
 				return CELL_OK;
 			}
 
